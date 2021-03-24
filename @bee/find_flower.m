9adb25ca53400bc18a5a1f84b5ecc36d4490806e
Jake Holland <hollandjake@hotmail.co.uk>
2021-03-24 14:59:40 +0000

    Working by slow

--- @bee/find_flower.m
+++ @bee/find_flower.m
@@ -5,38 +5,38 @@ function target = find_flower(bee)
 	% returns a coordinate [x, y] indicating the target that has been found
 
 	global ENV_DATA;
-		
+
+	target = [];
+
 	bee_pos = round(bee.pos);
 	
 	radius = bee.sensing_radius;
 	radius_sqr = radius ^ 2;
 	
-	mins = constrain(bee_pos-radius, 1, ENV_DATA.bm_size);	
+	mins = constrain(bee_pos-radius, 1, ENV_DATA.bm_size);
+	maxs = constrain(bee_pos+radius, 1, ENV_DATA.bm_size);
+
+	minX = mins(1);
+	maxX = maxs(2);
+	minY = mins(2);
+	maxY = maxs(2);
 	bee_offset = bee_pos - mins;
 
-	flower_positions = ENV_DATA.flower_positions;
+	search_field = ENV_DATA.pollen(minY:maxY,minX:maxX);
+	search_field(bee_offset + [1,1]) = 0; % Stop the bee going to the cell it is over
 	
-	x = flower_positions(:,1);
-	y = flower_positions(:,2);
-		
-	squared_dist = (x-bee_offset(:,1)).^2+(y-bee_offset(:,2)).^2;
-	
-	flower_positions = flower_positions(squared_dist < radius_sqr,:);
-	
-	x = flower_positions(:,2);
-	y = flower_positions(:,1);
-	indicies = flower_positions(:,3);
-	
-	poll = ENV_DATA.pollen(indicies);
-	matching_indicies = poll > 0;
-	squared_dist = squared_dist(matching_indicies);
-	if ~isempty(squared_dist)
-		x = x(matching_indicies);
-		y = y(matching_indicies);
-		n = ceil(min(1 + abs(randn), size(squared_dist,1)));
-		[~, idx] = topkrows(squared_dist, n, 'ascend');
-		t = [x(idx(end)),y(idx(end))];
+	[y,x] = find(search_field > 0); % Find all cells containing flowers with pollen levels > 0
 
-		target = mins - [1,1] + t;
+	squared_dist = (x-bee_offset(:,1)).^2+(y-bee_offset(:,2)).^2;
+	i = squared_dist < radius_sqr;
+	squared_join = [x(i),y(i),squared_dist(i)];
+	if ~isempty(squared_join)
+		sorted = sortrows(squared_join,3);
+		i = ceil(min(1 + abs(normrnd(0,1)), size(sorted,1)));
+		c = sorted(i,:);
+		x = c(1);
+		y = c(2);
+
+		target = mins - [1,1] + [x,y];
 	end
 end
