986ac3215d70bd31c8c6ad72df5b42a362cf530b
Jake Holland <hollandjake@hotmail.co.uk>
2021-03-24 15:46:23 +0000

    Speed improvement

--- @bee/find_flower.m
+++ @bee/find_flower.m
@@ -6,37 +6,27 @@ function target = find_flower(bee)
 
 	global ENV_DATA;
 
-	target = [];
 
 	bee_pos = round(bee.pos);
 	
 	radius = bee.sensing_radius;
-	radius_sqr = radius ^ 2;
+	radius_sqr = radius^2;
 	
-	mins = constrain(bee_pos-radius, 1, ENV_DATA.bm_size);
-	maxs = constrain(bee_pos+radius, 1, ENV_DATA.bm_size);
-
-	minX = mins(1);
-	maxX = maxs(2);
-	minY = mins(2);
-	maxY = maxs(2);
-	bee_offset = bee_pos - mins;
-
-	search_field = ENV_DATA.pollen(minY:maxY,minX:maxX);
-	search_field(bee_offset + [1,1]) = 0; % Stop the bee going to the cell it is over
+	flower_positions = ENV_DATA.flower_positions;
+	search_field = ENV_DATA.pollen(flower_positions(:, 3));
+	indices = search_field > 0;
+	flower_pos = flower_positions(indices,1:2);
+	distance_offset = flower_pos-bee_pos;
+	n_in_range = size(distance_offset,2);
+	target_index = ceil(max(1,min(abs(normrnd(1,1)), n_in_range)));
+	[d,i] = pdist2(distance_offset,zeros(1,n_in_range), 'squaredeuclidean', 'Smallest', target_index);
 	
-	[y,x] = find(search_field > 0); % Find all cells containing flowers with pollen levels > 0
-
-	squared_dist = (x-bee_offset(:,1)).^2+(y-bee_offset(:,2)).^2;
-	i = squared_dist < radius_sqr;
-	squared_join = [x(i),y(i),squared_dist(i)];
-	if ~isempty(squared_join)
-		sorted = sortrows(squared_join,3);
-		i = ceil(min(1 + abs(normrnd(0,1)), size(sorted,1)));
-		c = sorted(i,:);
-		x = c(1);
-		y = c(2);
-
-		target = mins - [1,1] + [x,y];
+	for idx = target_index:-1:1
+		cc = i(idx);
+		if d(idx) < radius_sqr
+			target = flower_pos(cc,:);
+			return
+		end
 	end
+	target = [];
 end
